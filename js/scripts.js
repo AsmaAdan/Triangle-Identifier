313e713db6da9bbe5feb9548fad77a9f743d49a4
AsmaAdan <asmaahmed.adan@gmail.com>
2019-02-15 17:00:02 +0300

    link between the html and the javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,32 @@
-var typesOfTriangles = ["Equilateral triangle", "Isosceles triangle", "Scalene triangle"];
-function triangleIdentifier (base, height, length) {
-	base = parseInt(prompt("Enter your base"));
- 	height = parseInt(prompt("Enter your height"));
-	length = parseInt(prompt("Enter your length"));
+function triangleIdentifier() {
+  var base1 = document.getElementById("base").value;
+  var height1 = document.getElementById("height").value;
+  var length1 = document.getElementById("length").value;
 
-if (base <= 0|| height <= 0|| length <= 0) {
-	alert("Not a triangle"); //* Meant to reject detect a zero or a negative value
+  var base = parseInt(base1);
+  var height = parseInt(height1);
+  var length = parseInt(length1);
+
+  var typesOfTriangles = ["Equilateral triangle", "Isosceles triangle", "Scalene triangle"];
+  var output = document.getElementById("output")
+
+  if (isNaN(base)|| isNaN(base)||isNaN(base)){
+  document.getElementById("output").innerHTML="You have not put any numbers";
 }
-else if (height == base && base == length) {
-	alert(typesOfTriangles[0]); //* Meant to detect if it an Equilateral triangle
+
+  if (base <= 0|| height <= 0|| length <= 0) {
+	document.getElementById("output").innerHTML="Not a triangle"; //* Meant to reject detect a zero or a negative value
+}
+  else if (height == base && base == length && height == length) {
+	   document.getElementById("output").innerHTML=typesOfTriangles[0]; //* Meant to detect if it an Equilateral triangle
  }
-else if ( height == base || height == length) {
-	alert(typesOfTriangles[1]); //* Meant to detect if it is an Isosceles triangle
+  else if ( height == base || height == length || length == base ) {
+	   document.getElementById("output").innerHTML= typesOfTriangles[1]; //* Meant to detect if it is an Isosceles triangle
 }
-else if (height!=base || base!=length) {
-	alert(typesOfTriangles[2]); //* Meant to detect if it is a scalene triangle
+  else if (height!=base || base!=length || length!=heigh && base + height > length || base +length > height || length + height > base) {
+	   document.getElementById("output").innerHTML=typesOfTriangles[2]; //* Meant to detect if it is a scalene triangle
 }
-else {
-	alert("Another type of triangle, requires angle input")
+  else {
+	   document.getElementById("output").innerHTML="Another type of triangle, requires Angle input";
 }
 }
-triangleIdentifier();
