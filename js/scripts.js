f9f7c7a125af0d6badd23b5fd8cc4725e6b78a59
AsmaAdan <asmaahmed.adan@gmail.com>
2019-02-16 21:23:46 +0300

    add changes to the spacing and indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 function triangleIdentifier() {
+
   var base1 = document.getElementById("base").value;
   var height1 = document.getElementById("height").value;
   var length1 = document.getElementById("length").value;
@@ -10,23 +11,22 @@ function triangleIdentifier() {
   var typesOfTriangles = ["Equilateral triangle", "Isosceles triangle", "Scalene triangle"];
   var output = document.getElementById("output")
 
-
   if (base <= 0|| height <= 0|| length <= 0) {
 	   document.getElementById("output").innerHTML="These values can't make a triangle"; //* Meant to reject detect a zero or a negative value
 }
-  else if (base + height < length || base +length < height || length + height < base){
-  document.getElementById("output").innerHTML="These values don't make a triangle"; //* Meant to reject values which may not make a triangle
+    else if (base + height < length || base +length < height || length + height < base){
+      document.getElementById("output").innerHTML="These values don't make a triangle"; //* Meant to reject values which may not make a triangle
 }
-  else if (height == base && base == length && height == length) {
-	   document.getElementById("output").innerHTML="This is an " + typesOfTriangles[0]+ "(image 2)"; //* Meant to detect if it an Equilateral triangle
+    else if (height == base && base == length && height == length) {
+	    document.getElementById("output").innerHTML="This is an " + typesOfTriangles[0]+ "(image 2)"; //* Meant to detect if it an Equilateral triangle
  }
-  else if ( height == base || height == length || length == base ) {
-	   document.getElementById("output").innerHTML= "This is an " + typesOfTriangles[1]; //* Meant to detect if it is an Isosceles triangle
+    else if ( height == base || height == length || length == base ) {
+	    document.getElementById("output").innerHTML= "This is an " + typesOfTriangles[1]; //* Meant to detect if it is an Isosceles triangle
 }
-  else if (height!=base || base!=length || length!=heigh && base + height > length || base +length > height || length + height > base) {
-	   document.getElementById("output").innerHTML="This is a " + typesOfTriangles[2]; //* Meant to detect if it is a scalene triangle
+    else if (height!=base || base!=length || length!=heigh && base + height > length || base +length > height || length + height > base) {
+	    document.getElementById("output").innerHTML="This is a " + typesOfTriangles[2]; //* Meant to detect if it is a scalene triangle
 }
-  else {
-	   document.getElementById("output").innerHTML="Another type of triangle, requires Angle input";
+    else {
+	    document.getElementById("output").innerHTML="Another type of triangle, requires Angle input";
 }
 }
